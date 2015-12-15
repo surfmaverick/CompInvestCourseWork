c9899eb82b4d239f2e0a988f5b57ee449e107d4a
wangzhe3224 <wangzhetju@gmail.com>
2015-12-15 22:40:16 +0000

    c1

--- courseWork1.py
+++ courseWork1.py
@@ -22,7 +22,7 @@ def simulate(start_date, end_date, symbols_list, allocation_list):
     day_time = dt.timedelta(hours=16)
     ldt_timestamps = du.getNYSEdays(start_date, end_date, day_time)
 
-    c_dataobj = da.DataAccess('Yahoo')
+    c_dataobj = da.DataAccess('Yahoo', cachestalltime=0)
     ls_keys = ['close']
     ldf_data = c_dataobj.get_data(ldt_timestamps, symbols_list, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
@@ -33,32 +33,55 @@ def simulate(start_date, end_date, symbols_list, allocation_list):
     # calculate daily return
     na_rets = normalized_price.copy()
     tsu.returnize0(na_rets)
-    #total_daily_ret = np.sum(na_rets, axis = 1)
-    total_daily_ret = na_rets[:,0]
-    
+
+    total_daily_ret = 0
+    for i in range(len(na_rets[0,:])):
+        total_daily_ret += na_rets[:, i] * allocation_list[i]
+
     # calculate standard deviation
     vol = np.std(total_daily_ret)
-    print 'standard deviation: ', vol
 
     # calcilate mean of daily return
     mean = np.mean(total_daily_ret)
-    print 'mean: ', mean
 
     # sharpe retio
     sharpe = np.sqrt(len(total_daily_ret))*mean/vol
-    print 'shaepe: ', sharpe
+    #sharpe = np.sqrt(252)*mean/vol
 
     # cumulative return
-    cum_ret = np.sum(total_daily_ret)
-    print 'cumulative return: ', cum_ret
+    def cumret(t, lf_returns):
+        """
+
+        :rtype: object
+        """
+        if t == 0:
+            return (1 + lf_returns[0])
+
+        return (cumret(t-1, lf_returns) * (1 + lf_returns[t]))
+
+    cum_ret = cumret(total_daily_ret.size - 1, total_daily_ret)
+
+    print 'Start date: ', start_date
+    print 'End dare: ', end_date
+    print 'Symbols: ', symbols_list
+    print 'Optimal allocation: ', allocation_list
+    print 'Sharpe Ratio: ', sharpe
+    print 'Volatility (stdev of daily returns): ', vol
+    print 'Average Daily Return: ', mean
+    print 'Cumulative Return: ', cum_ret
 
     return vol, total_daily_ret, sharpe, cum_ret
-
 
 
 if __name__ == "__main__":
     # Test function
     # inputs:
+    '''
+    start_date = dt.datetime(2010, 1, 1)
+    end_date = dt.datetime(2010, 12, 31)
+    symbols_list = ['AXP', 'HPQ', 'IBM', 'HNZ']
+    allocation_list = [0.0, 0.0, 0.0, 1.0]
+    '''
     start_date = dt.datetime(2011, 1, 1)
     end_date = dt.datetime(2011, 12, 31)
     symbols_list = ['AAPL', 'GLD', 'GOOG', 'XOM']
@@ -66,4 +89,3 @@ if __name__ == "__main__":
 
     vol, daily_ret, sharpe, cum_ret = simulate(start_date, end_date,
                                                symbols_list, allocation_list)
-
